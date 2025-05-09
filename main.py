e12cdc60d65f4da70f1a4d8f303091ea457a5875
jsdiaz27 <juan_s.diaz_v@uao.edu.co>
2025-05-08 20:07:15 -0500

    clase 8 de mayo pagina

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask,request, render_template
+from flask import Flask,request,render_template,make_response
 
 app=Flask(__name__)
 
-if __name__=="__main__":
-    app.run(debug=True)
+listado=[]
 
 @app.route('/buscar')
 def buscar():
@@ -22,5 +21,42 @@ def ruta_formulario():
 def ruta_registro():
     nombre=request.form.get("estudiante")
     email=request.form.get("correo")
-    passwd=request.form.get("contrasena")
-    return f"usuario resgistrado: {nombre}, {email}, {passwd}"
+    password=request.form.get("contrasena")
+    if nombre and email and password:
+        listado.append({"nombre":nombre,"correo":email,"password":password,})
+        return render_template("formulario.html", listado=listado)
+    
+    return f"faltan datos"
+
+    #return f"usuario resgistrado: {nombre}, {email}, {password}"
+
+#tipo de solicitud 3: Parametros en la ruta
+
+@app.route("/ropa/<string:producto>")
+def ruta_consulta(producto, talla):
+    return f"el producto consultado es {producto}, {talla}"
+
+#tipo de solicitud 4: headers http
+
+@app.route("/ver-headers")
+def ruta_headers():
+    navegador=request.headers.get("User-agent")
+    return f"el navegador que hace la peticion es {navegador}"
+
+#tipo de solicitud 5: manejo de cookies 
+
+@app.route("/crear-cookie")
+def crear_cookie():
+    respuesta=make_response("cookie-creada")
+    respuesta.set_cookie("usuario_logeado","true")
+    return respuesta
+
+
+@app.route("/leer-cookie")
+def leer_cookie():
+    valor=request.cookies.get("usuario_logeado")
+    return f"el valor de la cookie es: {valor}"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
